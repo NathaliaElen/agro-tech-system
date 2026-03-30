0829b3a6d28fcd0a9f3037d79232dca64ed47237
Nathália Elen <n.fialho.de.araujo@avanade.com>
2026-03-30 11:20:21 -0300

    chore: criação inicial do projeto

---
+++ src/main/java/com/agro/tech/system/agrotech/AgrotechApplication.java
@@ -0,0 +1,13 @@
+package com.agro.tech.system.agrotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrotechApplication.class, args);
+	}
+
+}
