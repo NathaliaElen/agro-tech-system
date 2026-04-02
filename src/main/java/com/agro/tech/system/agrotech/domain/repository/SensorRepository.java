6fe1582d1e2edec7bd3c4b6ccc74f96052b2f1e4
Nathália Elen <n.fialho.de.araujo@avanade.com>
2026-04-02 16:48:08 -0300

    feat: cria interfaces de repository no domínio

---
+++ src/main/java/com/agro/tech/system/agrotech/domain/repository/SensorRepository.java
@@ -0,0 +1,20 @@
+package com.agro.tech.system.agrotech.domain.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.agro.tech.system.agrotech.domain.model.Sensor;
+
+public interface SensorRepository {
+	Sensor salvar(Sensor sensor);
+	
+	Optional<Sensor> buscarPorId(String id);
+
+    Optional<Sensor> buscarPorCodigo(String codigo);
+
+    List<Sensor> buscarTodos();
+
+    List<Sensor> buscarPorAreaId(String areaId);
+
+    void deletar(String id);
+}
