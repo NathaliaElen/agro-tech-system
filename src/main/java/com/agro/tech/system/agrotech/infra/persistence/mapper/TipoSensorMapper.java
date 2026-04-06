24d4d6efa5eadd686712e960e9287b6fb5f73028
Nathália Elen <n.fialho.de.araujo@avanade.com>
2026-04-06 16:29:36 -0300

    refactor(infra): cria camada de mapper e remove lógica de conversão dos adapters

---
+++ src/main/java/com/agro/tech/system/agrotech/infra/persistence/mapper/TipoSensorMapper.java
@@ -0,0 +1,32 @@
+package com.agro.tech.system.agrotech.infra.persistence.mapper;
+
+import com.agro.tech.system.agrotech.domain.model.TipoSensor;
+import com.agro.tech.system.agrotech.infra.persistence.entity.TipoSensorEntity;
+
+public class TipoSensorMapper {
+	// "conversão" de entity em domain
+	public static TipoSensor toDomain(TipoSensorEntity entity) {
+		if (entity == null) {
+			return null;
+		}
+		
+		return new TipoSensor(			
+				entity.getId(),
+				entity.getNome(),
+				entity.getUnidadeMedida(),
+				entity.getDescricao(),
+				entity.getStatus()
+		);
+	}
+		
+	// "conversão" de domain em entity
+	public static TipoSensorEntity toEntity(TipoSensor tipoSensor) {
+		return new TipoSensorEntity(
+						tipoSensor.getId(),
+						tipoSensor.getNome(),
+						tipoSensor.getUnidadeMedida(),
+						tipoSensor.getDescricao(),
+						tipoSensor.getStatus()
+		);
+	}
+}
