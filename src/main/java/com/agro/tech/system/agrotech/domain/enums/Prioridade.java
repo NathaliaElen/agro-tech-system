527b213033a39d4d1602702507646c9b810ba868
Elizeu Barros <marcos.barros@outlook.com>
2026-04-06 14:52:47 -0300

    refactor: Adequação dos enums com o banco de dados

--- src/main/java/com/agro/tech/system/agrotech/domain/enums/Prioridade.java
+++ src/main/java/com/agro/tech/system/agrotech/domain/enums/Prioridade.java
@@ -1,11 +1,8 @@
 package com.agro.tech.system.agrotech.domain.enums;
 
 public enum Prioridade {
-	BAIXA,
-	
-	MEDIA,
-	
-	ALTA,
-	
-	CRITICA
+	BAIXA("B"),	
+	MEDIA("M"),	
+	ALTA("A"),	
+	CRITICA("C")
 }
